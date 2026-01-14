edcf227d198b2d651b8a32bd89b08da0218cfc1f
shaurya khandelwal <shauryakhandelwal0025@gmail.com>
2026-01-14 23:37:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showWish() {
+    document.getElementById("wish").innerText =
+        "on this day i tell you that you are the best sister anyone could ever ask for! because of that i have have a surprise for you! whcih u will find soon :)";
+}
